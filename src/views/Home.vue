7882169ec1221d08de83ff6367fa2726fcf63a6c
code-lad <codelad1@gmial.com>
2020-09-02 13:01:33 +0200

    hidden components based on screen sizes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,15 @@
 <template>
   <div class="home">
     <h2>Home Page</h2>
-    <v-btn class="blue white--text">click me now</v-btn>
+    <v-btn class="hidden-lg-only" color="green">Click me when you see me</v-btn>
     <p></p>
-    <v-btn depressed color="green">click me now</v-btn>
+    <v-btn class="hidden-sm-and-down" color="red">click me when you see me</v-btn>
     <p></p>
-    <v-btn flat color="red">click me now</v-btn>
+    <v-btn class="hidden-md-only" color="blue">click me when you see me</v-btn>
     <p></p>
-    <v-btn color="blue white--text">
-      <v-icon left>mdi-email</v-icon>
-      <span>send mail</span>
-    </v-btn>
+    <v-btn class="hidden-md-and-up" color="yellow">click me when you see me</v-btn>
     <p></p>
-    <v-btn small color="green white--text">
-      <v-icon>mdi-menu</v-icon>
-      <span left>MENU</span>
-    </v-btn>
-    <p></p>
-    <v-btn color="red white--text">large button</v-btn>
-    <p></p>
-    <v-btn fab color="green">
-      <v-icon>mdi-domain</v-icon>
-    </v-btn>
+    <v-btn class="hidden-md-and-down" color="purple">click me when you see me</v-btn>
   </div>
 </template>
 
